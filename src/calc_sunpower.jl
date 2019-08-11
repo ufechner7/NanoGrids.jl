e2863814fbff5122110c1b85ae3f8febe9b51bd5
Uwe Fechner <fechner@aenarete.eu>
2019-08-11 02:34:15 +0200

    cleanup

--- src/calc_sunpower.jl
+++ src/calc_sunpower.jl
@@ -1,7 +1,7 @@
 # Script to calculate the availabily of solar power over the year
 using DataFrames, FeatherFiles, Statistics 
 
-const AREA = 16.0 # area of the solar panels
+const AREA = 16.0 # area of the solar panels in m²
 
 function calc_average_power(df)
     return AREA * mean(df.Power_Generation)
@@ -13,15 +13,10 @@ function calc_rel_availability(df, min_power)
     return 100 * minpowercount / size(df)[1]
 end
 
-function calc_non_zero_percent(df)
-    return calc_rel_availability(df, 0.0)
-end
-
 if !isdefined(@__MODULE__, :df) df=DataFrame(load("data/solaryear.feather")) end
 
 av = calc_average_power(df)
 println("Average solar power [W]:                     ", round(av, digits=1))
-println("Percentage of solar power availability   [%]: ", round(calc_non_zero_percent(df), digits=1))
-println("Percentage of time for more than average [%]: ", round(calc_rel_availability(df, av), digits=1))
+println("Percentage of solar power availability   [%]: ", round(calc_rel_availability(df, 0.0), digits=1))
 println("Percentage of time for more than 50% av. [%]: ", round(calc_rel_availability(df, av/2), digits=1))
-
+println("Percentage of time for more than average [%]: ", round(calc_rel_availability(df, av), digits=1))
