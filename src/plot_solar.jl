de959e1b54894c9725bfca4bf21ee24a989ea7fd
Uwe Fechner <fechner@aenarete.eu>
2019-08-11 18:17:49 +0200

    added title

--- src/plot_solar.jl
+++ src/plot_solar.jl
@@ -5,4 +5,6 @@ AREA = 16.0 # area of the solar panels
 df = DataFrame(load("./data/solaryear.feather"))
 
 plot(df.Power_Generation*AREA)
+title("Solar power, 16 m², Gouda, NL")
+
 mean(df.Power_Generation*AREA)
